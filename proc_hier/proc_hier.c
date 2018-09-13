5c529e2ab89a21321528b77c7d22a98405978c3f
iiCloudex <marcoabbpm@hotmail.com>
2018-09-13 14:05:38 -0700

    proc finished

--- proc_hier/proc_hier.c
+++ proc_hier/proc_hier.c
@@ -7,57 +7,58 @@
 #include    <unistd.h>
 
 #define oops(m) {perror(m); exit(EXIT_FAILURE);}
-#define maxFork 2
-#define maxHier 6
+#define maxHier 3
 
 pid_t pid;
 char name[maxHier] = "1";
 
-void goFork(int forkVal)
+void goFork(int level)
 {
-	forkVal++;
-	if(forkVal < maxFork)
+	level++;
+	if(level < maxHier)
 	{
 		pid = fork();
+
 		if(pid < 0) //error
 		{
 			oops("Fork Failed!")
 		}
 		else if(pid == 0)
 		{
-			setName(".1", forkVal);
+			strcat(name, ".1");
+			printf("I am the child. Name: %s\n", name);
+			exit(EXIT_SUCCESS);
 		}
 		else
 		{
-			setName(".2", forkVal);
+			pid = fork();
+
+			if(pid < 0)
+			{
+				oops("Fork Failed!");
+			}
+			else if(pid == 0)
+			{
+				strcat(name, ".2");
+				printf("I am the child. Name: %s\n", name);
+				exit(EXIT_SUCCESS);
+			}
+			else
+			{
+				printf("I am the parent. Name: %s\n", name);
+				if (wait(NULL) < 0)
+					printf("-1 from wait(NULL) with errno = %d\n", errno);
+ 	    			printf("Child Complete\n");
+    	    			exit(EXIT_SUCCESS);
+			}
 		}
-	}
-
-}
-
-void setName(char endNum[maxFork], int forkVal)
-{
-	strcat(name, endNum);
-	pid = fork();
-	if(pid < 0)
-	{
-		oops("Fork Failed!");
-	}
-	else if(pid == 0)
-	{
-		goFork(forkVal);
-	}
-	else
-	{
-		if(execlp("./iam", "iam", name, NULL) < 0 )
-			oops("Execlp Failed!");
 	}
 
 }
 
 int main()
 {
-    int currFork = 0; 
+    int currLevel = 0; 
 
     // fork a child process
     pid = fork();
@@ -68,15 +69,32 @@ int main()
     }
     else if(pid == 0) // child
     {
-	    goFork(currFork);
+	    strcat(name, ".1");
+	    printf("I am the child. Name: %s\n", name);
+	    goFork(currLevel);
     }
     else
     {
-    	    /* parent will wait for the child to complete */
-    	    if (wait(NULL) < 0)
-		    printf("-1 from wait(NULL) with errno = %d\n", errno);
+	    pid = fork();
 
-    	    printf("Child Complete\n");
-    	    exit(EXIT_SUCCESS);
+	    if(pid < 0)
+	    {
+		    oops("Fork Failed!");
+	    }
+	    else if(pid == 0)
+	    {
+		    strcat(name, ".2");
+		    printf("I am the child. Name: %s\n", name);
+		    goFork(currLevel);
+	    }
+	    else
+	    {
+		printf("I am the parent. Name: %s\n", name);
+		/* parent will wait for the child to complete */
+    	    	if (wait(NULL) < 0)
+			printf("-1 from wait(NULL) with errno = %d\n", errno);
+ 	    	printf("Child Complete\n");
+    	    	exit(EXIT_SUCCESS);
+	    }
     }
 }
