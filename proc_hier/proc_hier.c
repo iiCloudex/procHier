fd368b19ee13d9491fccbf22a9cbacdb8d00fd52
iiCloudex <marcoabbpm@hotmail.com>
2018-09-12 20:49:34 -0700

    A few changes

--- proc_hier/proc_hier.c
+++ proc_hier/proc_hier.c
@@ -7,30 +7,76 @@
 #include    <unistd.h>
 
 #define oops(m) {perror(m); exit(EXIT_FAILURE);}
+#define maxFork 2
+#define maxHier 6
+
+pid_t pid;
+char name[maxHier] = "1";
+
+void goFork(int forkVal)
+{
+	forkVal++;
+	if(forkVal < maxFork)
+	{
+		pid = fork();
+		if(pid < 0) //error
+		{
+			oops("Fork Failed!")
+		}
+		else if(pid == 0)
+		{
+			setName(".1", forkVal);
+		}
+		else
+		{
+			setName(".2", forkVal);
+		}
+	}
+
+}
+
+void setName(char endNum[maxFork], int forkVal)
+{
+	strcat(name, endNum);
+	pid = fork();
+	if(pid < 0)
+	{
+		oops("Fork Failed!");
+	}
+	else if(pid == 0)
+	{
+		goFork(forkVal);
+	}
+	else
+	{
+		if(execlp("./iam", "iam", name, NULL) < 0 )
+			oops("Execlp Failed!");
+	}
+
+}
 
 int main()
 {
-    pid_t pid;
+    int currFork = 0; 
 
     // fork a child process
     pid = fork();
 
     if (pid < 0) // error occurred
-    oops("Fork Failed!");
-
-    if (pid == 0) // child
     {
-        printf("I am the child %d\n", getpid());
-        if (execlp("./iam", "iam", "Hello Parent! Please do not kill me!", NULL) < 0)
-        oops("Execlp Failed!");
+	    oops("Fork Failed!");
     }
+    else if(pid == 0) // child
+    {
+	    goFork(currFork);
+    }
+    else
+    {
+    	    /* parent will wait for the child to complete */
+    	    if (wait(NULL) < 0)
+		    printf("-1 from wait(NULL) with errno = %d\n", errno);
 
-    // pid > 0 ==> must be parent
-    printf("I am the parent %d\n", getpid());
-    /* parent will wait for the child to complete */
-    if (wait(NULL) < 0)
-        printf("-1 from wait(NULL) with errno = %d\n", errno);
-
-    printf("Child Complete\n");
-    exit(EXIT_SUCCESS);
+    	    printf("Child Complete\n");
+    	    exit(EXIT_SUCCESS);
+    }
 }
